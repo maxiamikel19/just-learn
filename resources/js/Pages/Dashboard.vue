50c1527f346ca45a3c877417a7862da03cc7a72d
maxiamikel19 <maxloversist19@gmail.com>
2024-05-31 11:08:22 -0300

    Middleware para publicar o Role e as Permissions do usuario no sistema

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -4,10 +4,10 @@ import Welcome from '@/Components/Welcome.vue';
 </script>
 
 <template>
-    <AppLayout title="Dashboard">
+    <AppLayout title="Dashboard ">
         <template #header>
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Dashboard
+                Dashboard 
             </h2>
         </template>
 
