3948bc30c351306c9d14730b5252f58559932973
maxiamikel19 <maxloversist19@gmail.com>
2024-05-30 23:49:11 -0300

    Implementar e definir Role e permissions de usuarios

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,11 +15,15 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
 
-        $this->call(LevelSeeder::class);
+        $this->call([
+            LevelSeeder::class,
+            RoleSeeder::class,
+            UserSeeder::class
+        ]);
     }
 }
