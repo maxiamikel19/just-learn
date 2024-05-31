dd00f7fb2da3cb62cbd1ceae7020de4e22bcb024
maxiamikel19 <maxloversist19@gmail.com>
2024-05-31 11:53:17 -0300

    Criar os Controllers e refatorar web.php

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,11 @@
 <?php
 
+use App\Http\Controllers\DashboadController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+Route::get('/', [DashboadController::class, 'index']);
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return Inertia::render('Dashboard');
-    })->name('dashboard');
+Route::middleware(['auth:sanctum',config('jetstream.auth_session'),'verified',])->group(function () {
+    Route::get('/dashboard', [DashboadController::class, 'dashboard'])->name('dashboard');
 });
