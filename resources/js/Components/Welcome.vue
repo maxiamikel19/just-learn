5013576b79321e70a61ec712106645f654d477be
maxiamikel19 <maxloversist19@gmail.com>
2024-06-02 07:16:29 -0300

    Implementar Catrgories GUI

--- resources/js/Components/Welcome.vue
+++ resources/js/Components/Welcome.vue
@@ -3,17 +3,7 @@ import ApplicationLogo from '@/Components/ApplicationLogo.vue';
 </script>
 
 <template>
-    Nzme: {{ $page.props.auth.user.name }}
-    <div>
-        <button v-if="$page.props.user.permissions.includes('create roles')">Manage roles</button>
-        <button v-if="$page.props.user.permissions.includes('create lessons')">Manage lessons</button>
-    </div>
+   
 </template>
 
-<style>
-    button{
-        background: #0099FF;
-        padding: 10px;
-        margin: 9px;
-    }
-</style>
+
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,16 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\DashboadController;
-use Illuminate\Foundation\Application;
+use App\Http\Controllers\LessonController;
+use App\Http\Controllers\RoleController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [DashboadController::class, 'index']);
 
 Route::middleware(['auth:sanctum',config('jetstream.auth_session'),'verified',])->group(function () {
     Route::get('/dashboard', [DashboadController::class, 'dashboard'])->name('dashboard');
+    Route::resource('/categories', CategoryController::class);
+    Route::resource('/lessons', LessonController::class);
+    Route::resource('/roles', RoleController::class);
 });
